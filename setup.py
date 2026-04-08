109245921c9491db619c7d5fc121db0567d114be
Mahesh Makvana <maheshmakvana@users.noreply.github.com>
2026-04-08 16:52:21 +0530

    Rename PyPI package to llmgrader (llmeval taken on PyPI)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="llmeval",
+    name="llmgrader",
     version="1.0.0",
     author="Mahesh Makvana",
     author_email="maheshmakvana@example.com",
